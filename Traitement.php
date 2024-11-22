644c45ddfac984abe5bbf18383b134941f5636d2
BadrMorad <moradpro989@gmail.com>
2024-11-22 17:50:35 +0100

    php

--- Traitement.php
+++ Traitement.php
@@ -17,22 +17,32 @@ if (isset($_POST['action']) && $_POST['action'] == 'ajouter') {
     }
 }
 
+// Gestion des actions via $_GET['action']
 if (isset($_GET['action'])) {
-    if ($_GET['action'] == 'supprimer' && isset($_GET['id'])) {
-        $id = $_GET['id'];
-        if (isset($_SESSION['produits'][$id])) {
-            unset($_SESSION['produits'][$id]);
-            $_SESSION['message'] = "Produit supprimé avec succès.";
-        }
-    } elseif ($_GET['action'] == 'supprimer_tous') {
-        $_SESSION['produits'] = [];
-        $_SESSION['message'] = "Tous les produits ont été supprimés.";
+    switch ($_GET['action']) {
+        case 'supprimer':
+            if (isset($_GET['id'])) {
+                $id = $_GET['id'];
+                if (isset($_SESSION['produits'][$id])) {
+                    unset($_SESSION['produits'][$id]);
+                    $_SESSION['message'] = "Produit supprimé avec succès.";
+                }
+            }
+            break;
+
+        case 'supprimer_tous':
+            $_SESSION['produits'] = [];
+            $_SESSION['message'] = "Tous les produits ont été supprimés.";
+            break;
+
+        default:
+            $_SESSION['message'] = "Action inconnue.";
+            break;
     }
 }
 
 header('Location: index.php');
 exit();
-
 
 
 
